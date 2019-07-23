d8c91be2c9522c4163ee5a81ca5e69e9ba34bbb7
candy <ryo98biswas@gmail.com>
2019-07-24 00:46:44 +0530

    Test ended for diff comp

--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -31,7 +31,6 @@ public class MainActivity extends AppCompatActivity {
     boolean isOkEmail = false;
     boolean isOkPwd = false;
     ProgressDialog progressDialog;
-    Int i;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
